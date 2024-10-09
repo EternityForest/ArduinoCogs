037e1720e536312a55b5157d6e0fbb61aca6600e
Daniel Dunn <dannydunn@example.com>
2024-10-08 19:32:41 -0600

    oops forgot the py file

--- build_web_data.py
+++ build_web_data.py
@@ -1,21 +1,23 @@
-import os
 import gzip
+import os
+
 
 def file_to_c_array(filename: str) -> str:
     with open(filename, "rb") as f:
         data = f.read()
     data = gzip.compress(data, mtime=os.path.getmtime(filename))
 
-    vn = os.path.basename(filename).replace('.', '_')+"_gz"
-    br = '{'
+    vn = os.path.basename(filename).replace(".", "_") + "_gz"
+    br = "{"
     c_array = f"const unsigned char {vn}[] = {br}\n"
 
     for i in range(0, len(data), 16):
-        line = ", ".join("0x{:02X}".format(b) for b in data[i:i+16])
+        line = ", ".join(f"0x{b:02X}" for b in data[i : i + 16])
         c_array += "    " + line + ",\n"
     c_array += "};\n"
 
     return c_array
+
 
 d = os.path.dirname(os.path.abspath(__file__))
 d2 = os.path.join(d, "builtinfiles")
@@ -25,7 +27,7 @@ os.makedirs(opd, exist_ok=True)
 
 if __name__ == "__main__":
     for fn in os.listdir(d2):
-        fn2 = os.path.basename(fn).replace('.', '_')+"_gz.h"
+        fn2 = os.path.basename(fn).replace(".", "_") + "_gz.h"
         c_array = file_to_c_array(os.path.join(d2, fn))
         with open(os.path.join(opd, os.path.basename(fn2)), "w") as f:
             f.write(c_array)
